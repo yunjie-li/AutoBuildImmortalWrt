8e6efcf5c8c0e7aeb12524b735bddfc4bbc5271d
yunjie <yunjie-li@users.noreply.github.com>
2025-02-27 00:24:30 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -8,6 +8,19 @@ echo "Include Docker: $INCLUDE_DOCKER"
 echo "doanload external packages"
 cat /home/build/immortalwrt/files/external-package-urls.txt | xargs wget -P /home/build/immortalwrt/packages
 
+echo "Start oh-my-zsh Config !"
+echo "Current Path: $PWD"
+mkdir -p $PWD/files/root/.oh-my-zsh
+ROOT=$PWD/files/root
+ZSH=$ROOT/.oh-my-zsh
+ZSH_CUSTOM=$ZSH/custom
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git $ZSH
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cp $ZSH/templates/zshrc.zsh-template $ROOT/.zshrc
+sed -i "s/ZSH_THEME=\".\+\"/ZSH_THEME=\"ys\"/" $ROOT/.zshrc
+sed -i "s/plugins=.\+/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" $ROOT/.zshrc
+
 echo "Create pppoe-settings"
 mkdir -p  /home/build/immortalwrt/files/etc/config
 
