a20a39d9f2fd7106459089c3bb4f852efa6c65b5
yunjie <yunjie-li@users.noreply.github.com>
2025-02-28 00:13:32 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
-# Log file for debugging
-LOGFILE="/tmp/uci-defaults-log.txt"
-echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
+# # Log file for debugging
+# LOGFILE="/tmp/uci-defaults-log.txt"
+# echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 # uci set firewall.@zone[1].input='ACCEPT'
 
@@ -12,13 +12,13 @@ echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
-SETTINGS_FILE="/etc/config/pppoe-settings"
-if [ ! -f "$SETTINGS_FILE" ]; then
-    echo "PPPoE settings file not found. Skipping." >> $LOGFILE
-else
-   # 读取pppoe信息($enable_pppoe、$pppoe_account、$pppoe_password)
-   . "$SETTINGS_FILE"
-fi
+# SETTINGS_FILE="/etc/config/pppoe-settings"
+# if [ ! -f "$SETTINGS_FILE" ]; then
+#     echo "PPPoE settings file not found. Skipping." >> $LOGFILE
+# else
+#    # 读取pppoe信息($enable_pppoe、$pppoe_account、$pppoe_password)
+#    . "$SETTINGS_FILE"
+# fi
 
 # # 计算网卡数量
 # count=0
@@ -68,40 +68,40 @@ fi
 #       echo "ports of device 'br-lan' are update." >> $LOGFILE
 #    fi
 # LAN口设置静态IP
-uci set network.lan.proto='static'
-# 多网口设备 支持修改为别的ip地址
-uci set network.lan.ipaddr='192.168.2.10'
-uci set network.lan.netmask='255.255.255.0'
-echo "set 192.168.2.10 at $(date)" >> $LOGFILE
-# 判断是否启用 PPPoE
-echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
-if [ "$enable_pppoe" = "yes" ]; then
-  echo "PPPoE is enabled at $(date)" >> $LOGFILE
-  # 设置ipv4宽带拨号信息
-  uci set network.wan.proto='pppoe'
-  uci set network.wan.username=$pppoe_account
-  uci set network.wan.password=$pppoe_password
-  uci set network.wan.peerdns='1'
-  uci set network.wan.auto='1'
-  # 设置ipv6 默认不配置协议
-  uci set network.wan6.proto='none'
-  echo "PPPoE configuration completed successfully." >> $LOGFILE
-else
-  echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
-fi
+# uci set network.lan.proto='static'
+# # 多网口设备 支持修改为别的ip地址
+# uci set network.lan.ipaddr='192.168.2.10'
+# uci set network.lan.netmask='255.255.255.0'
+# echo "set 192.168.2.10 at $(date)" >> $LOGFILE
+# # 判断是否启用 PPPoE
+# echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
+# if [ "$enable_pppoe" = "yes" ]; then
+#   echo "PPPoE is enabled at $(date)" >> $LOGFILE
+#   # 设置ipv4宽带拨号信息
+#   uci set network.wan.proto='pppoe'
+#   uci set network.wan.username=$pppoe_account
+#   uci set network.wan.password=$pppoe_password
+#   uci set network.wan.peerdns='1'
+#   uci set network.wan.auto='1'
+#   # 设置ipv6 默认不配置协议
+#   uci set network.wan6.proto='none'
+#   echo "PPPoE configuration completed successfully." >> $LOGFILE
+# else
+#   echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
+# fi
 # fi
 
 
 # 设置所有网口可访问网页终端
-uci delete ttyd.@ttyd[0].interface
+# uci delete ttyd.@ttyd[0].interface
 
 # 设置所有网口可连接 SSH
-uci set dropbear.@dropbear[0].Interface=''
-uci commit
+# uci set dropbear.@dropbear[0].Interface=''
+# uci commit
 
-# 设置编译作者信息
-FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wwmm"
-sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
+# # 设置编译作者信息
+# FILE_PATH="/etc/openwrt_release"
+# NEW_DESCRIPTION="Compiled by wwmm"
+# sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
-exit 0
+# exit 0
