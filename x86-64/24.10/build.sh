083a907f33a80abb650d1002f732a82dcc6c005b
yunjie <yunjie-li@users.noreply.github.com>
2025-02-28 00:14:55 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -77,6 +77,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES git"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
